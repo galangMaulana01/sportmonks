8a1c37d82e2f4718ae3b4e67bfb74d8999614c50
galangMaulana01 <maulanagebril99@gmail.com>
2026-06-08 21:57:43 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import httpx
 import os
 
@@ -7,14 +7,14 @@ app = FastAPI()
 SPORTMONKS_TOKEN = os.getenv("SPORTMONKS_API_TOKEN")
 BASE_URL = "https://api.sportmonks.com/v3/football"
 
+
 @app.get("/")
 async def root():
-    return {
-        "message": "active"
-    }
+    return {"message": "active"}
+
 
 @app.get("/leagues/{league_id}")
-async def league_season(league_id: int):
+async def league(league_id: int):
     async with httpx.AsyncClient() as client:
         response = await client.get(
             f"{BASE_URL}/leagues/{league_id}",
@@ -25,6 +25,7 @@ async def league_season(league_id: int):
         )
         response.raise_for_status()
         return response.json()
+
 
 @app.get("/seasons/{season_id}")
 async def season(season_id: int):
@@ -38,8 +39,9 @@ async def season(season_id: int):
         response.raise_for_status()
         return response.json()
 
-@app.get("/standings/seasons/{season_id}/")
-async def season_standings(season_id: int):
+
+@app.get("/standings/seasons/{season_id}")
+async def standing(season_id: int):
     async with httpx.AsyncClient() as client:
         response = await client.get(
             f"{BASE_URL}/standings/seasons/{season_id}",
@@ -50,17 +52,19 @@ async def season_standings(season_id: int):
         response.raise_for_status()
         return response.json()
 
-@app.get("/fixtures/{team_id}")
-async def fixture(team_id: int):
+
+@app.get("/teams/{team_id}")
+async def team(team_id: int):
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            f"{BASE_URL}/fixtures/{team_id}",
+            f"{BASE_URL}/teams/{team_id}",
             params={
                 "api_token": SPORTMONKS_TOKEN
             }
         )
         response.raise_for_status()
         return response.json()
+
 
 @app.get("/fixtures/{fixture_id}")
 async def fixture(fixture_id: int):
